0549b9c2e174bd2cf46611e9da0fc5f111479592
MedivhGO <lijingqi93@gmail.com>
2022-09-02 08:33:55 +0800

    format code

--- src/MyString.cpp
+++ src/MyString.cpp
@@ -143,7 +143,6 @@ int MyString::operator()(const MyString& substr) const {
     // KMP
     const MyString& ss = *this;
     const MyString& pp = substr;
-
     int n = ss.length(), m = pp.length();
     if(m == 0) return 0;
     //设置哨兵
@@ -151,16 +150,16 @@ int MyString::operator()(const MyString& substr) const {
     MyString p = " " + pp;
     vector<int> next(m + 1);
     //预处理next数组
-    for(int i = 2, j = 0; i <= m; i++){
-        while(j and p[i] != p[j + 1]) j = next[j];
-        if(p[i] == p[j + 1]) j++;
+    for(int i = 2, j = 0; i <= m; i++) {
+        while(j and p[i] != p[j + 1]) { j = next[j]; }
+        if(p[i] == p[j + 1]) { j++; }
         next[i] = j;
     }
     //匹配过程
-    for(int i = 1, j = 0; i <= n; i++){
-        while(j and s[i] != p[j + 1]) j = next[j];
-        if(s[i] == p[j + 1]) j++;
-        if(j == m) return i - m;
+    for(int i = 1, j = 0; i <= n; i++) {
+        while(j and s[i] != p[j + 1]) { j = next[j]; }
+        if(s[i] == p[j + 1]) { j++; }
+        if(j == m) { return i - m; }
     }
     return -1;
 }
