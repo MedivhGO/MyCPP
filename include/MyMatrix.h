82a5eed1a82d7cf62bfde568c55b2ff4359eadd8
MedivhGO <lijingqi93@gmail.com>
2022-09-02 12:07:24 +0800

    add code to MyMatrixClass

--- include/MyMatrix.h
+++ include/MyMatrix.h
@@ -7,6 +7,7 @@
 
 #include <vector>
 #include <iostream>
+#include <random>
 
 using std::vector;
 using std::cout;
@@ -17,9 +18,15 @@ class MyMatrix {
 public:
     MyMatrix();
 
-    MyMatrix(unsigned row, unsigned col); // 指定行列构造
+    MyMatrix(unsigned row, unsigned col);
 
-    MyMatrix(vector<T> &r); // 由vector作为行向量构造矩阵
+    MyMatrix(int dimension);
+
+    MyMatrix(vector<T> &r);
+
+    MyMatrix(const std::initializer_list<T>& il);
+
+    MyMatrix(const std::initializer_list<std::initializer_list<T>>& il);
 
     ~MyMatrix();
 
@@ -31,29 +38,95 @@ public:
 
     unsigned getCol() const;
 
-    MyMatrix<T> getRowMyMatrix(unsigned r) const; //取第r行向量
+    MyMatrix<T> getRowMyMatrix(unsigned r) const;
 
-    MyMatrix<T> getColMyMatrix(unsigned c) const; //取第c列向量
+    MyMatrix<T> getColMyMatrix(unsigned c) const;
+
+    void randomFillMatrix(int low, int hight);
 
     void showMatrix();
 
-    friend MyMatrix<T> operator+(const MyMatrix<T> &A, const MyMatrix<T> &B); //矩阵加法
+    void showDiagonal();
 
-    friend MyMatrix<T> operator-(const MyMatrix<T> &A, const MyMatrix<T> &B); //矩阵减法
+    bool isSquare() const;
 
-    friend MyMatrix<T> operator*(const MyMatrix<T> &A, const MyMatrix<T> &B);  // 矩阵乘法
+    friend MyMatrix<T> operator+(const MyMatrix<T> &A, const MyMatrix<T> &B) {
+        MyMatrix<T> C(A.getRow(), A.getCol());
+        for (unsigned i = 0; i < C.getRow(); i++) {
+            for (unsigned j = 0; j < C.getCol(); j++) {
+                C[i][j] = A[i][j] + B[i][j];
+            }
+        }
+        return C;
+    }
 
-    friend MyMatrix<T> operator*(const T &e, const MyMatrix<T> &A);            // 矩阵数乘
+    friend MyMatrix<T> operator-(const MyMatrix<T> &A, const MyMatrix<T> &B) {
+        MyMatrix<T> C(A.getRow(), A.getCol());
+        for (unsigned i = 0; i < C.getRow(); i++) {
+            for (unsigned j = 0; j < C.getCol(); j++) {
+                C[i][j] = A[i][j] - B[i][j];
+            }
+        }
+        return C;
+    }
 
-    friend MyMatrix<T> operator/(const MyMatrix<T> &A, const T &e);            // 矩阵除以一个数
+    friend MyMatrix<T> operator*(const MyMatrix<T> &A, const MyMatrix<T> &B) {
+        unsigned i, j, k;
+        MyMatrix<T> C(A.getRow(), B.getCol());
+        for (i = 0; i < A.getRow(); i++) {
+            for (j = 0; j < B.getCol(); j++) {
+                for (k = 0; k < A.getCol(); k++) {
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+        return C;
+    }
 
-    bool isSquare() const;  //判断是否为方阵
+    friend MyMatrix<T> operator*(const T &e, const MyMatrix<T> &A) {
+        MyMatrix<T> C(A.getRow(), A.getCol());
+        for (unsigned i = 0; i < C.getRow(); i++) {
+            for (unsigned j = 0; j < C.getCol(); j++) {
+                C[i][j] = A[i][j] * e;
+            }
+        }
+        return C;
+    }
+
+    friend MyMatrix<T> operator/(const MyMatrix<T> &A, const T &e) {
+        MyMatrix<T> C(A.getRow(), A.getCol());
+        for (unsigned i = 0; i < C.getRow(); i++) {
+            for (unsigned j = 0; j < C.getCol(); j++) {
+                C[i][j] = A[i][j] / e;
+            }
+        }
+        return C;
+    }
+
+    friend bool operator==(const MyMatrix<T> &A, const MyMatrix<T> &B) {
+        if (A.getCol() != B.getCol() || A.getRow() != B.getRow()) {
+            return false;
+        }
+        for (unsigned i = 0; i < A.getRow(); i++) {
+            for (unsigned j = 0; j < A.getCol(); j++) {
+                if (A[i][j] != B[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    friend bool operator!=(const MyMatrix<T> &A, const MyMatrix<T> &B) {
+        return !(A == B);
+    }
 
 private:
     void setDim(unsigned row, unsigned col);
 
 private:
     vector<vector<T>> m_elem;
+    int m_dimension;
 };
 
 template<typename T>
@@ -67,9 +140,32 @@ MyMatrix<T>::MyMatrix(unsigned row, unsigned col) {
 }
 
 template<typename T>
+MyMatrix<T>::MyMatrix(int dimension) {
+    this->m_dimension = dimension;
+    this->m_elem.resize(dimension);
+    for (int i = 0; i < dimension; ++i) {
+        m_elem[i].resize(dimension);
+    }
+}
+
+template<typename T>
 MyMatrix<T>::MyMatrix(vector<T> &r) {
     setDim(r.size(), 1);
     m_elem[0] = r;
+}
+
+template<typename T>
+MyMatrix<T>::MyMatrix(const std::initializer_list<T>& il) {
+    setDim(il.size(), 1);
+    m_elem[0] = il;
+}
+
+template<typename T>
+MyMatrix<T>::MyMatrix(const std::initializer_list<std::initializer_list<T>>& il) {
+    std::vector<std::initializer_list<T> > setVec = il;
+    for (typename std::vector<std::initializer_list<T> >::iterator i = setVec.begin(); i != setVec.end(); i++) {
+        m_elem.push_back(std::vector<T>(*i));
+    }
 }
 
 template<typename T>
@@ -129,6 +225,18 @@ MyMatrix<T> MyMatrix<T>::getColMyMatrix(unsigned c) const {
 }
 
 template<typename T>
+void MyMatrix<T>::randomFillMatrix(int low, int high) {
+    std::random_device r;
+    std::mt19937 mtgen(r());
+    std::uniform_int_distribution<> dist(low, high);
+    for (int i = 0; i < getRow(); ++i) {
+        for (int j = 0; j < getCol(); ++j) {
+            m_elem[i][j] = dist(mtgen);
+        }
+    }
+}
+
+template<typename T>
 void MyMatrix<T>::showMatrix() {
     for (unsigned i = 0; i < m_elem.size(); i++) {
         for (unsigned j = 0; j < m_elem[i].size(); j++) {
@@ -136,69 +244,23 @@ void MyMatrix<T>::showMatrix() {
         }
         cout << endl;
     }
+    cout << endl;
+}
+
+template<typename T>
+void MyMatrix<T>::showDiagonal() {
+    if (!isSquare()) {
+        return;
+    }
+    for (unsigned i = 0; i < m_elem.size(); i++) {
+        cout << m_elem[i][i] << " ";
+    }
+    cout << endl;
 }
 
 template<typename T>
 bool MyMatrix<T>::isSquare() const {
     return getRow() == getCol();
-}
-
-template<typename T>
-MyMatrix<T> operator+(const MyMatrix<T> &A, const MyMatrix<T> &B) {
-    MyMatrix<T> C(A.getRow(), A.getCol());
-    for (unsigned i = 0; i < C.getRow(); i++) {
-        for (unsigned j = 0; j < C.getCol(); j++) {
-            C[i][j] = A[i][j] + B[i][j];
-        }
-    }
-    return C;
-}
-
-template<typename T>
-MyMatrix<T> operator-(const MyMatrix<T> &A, const MyMatrix<T> &B) {
-    MyMatrix<T> C(A.getRow(), A.getCol());
-    for (unsigned i = 0; i < C.getRow(); i++) {
-        for (unsigned j = 0; j < C.getCol(); j++) {
-            C[i][j] = A[i][j] - B[i][j];
-        }
-    }
-    return C;
-}
-
-template<typename T>
-MyMatrix<T> operator*(const MyMatrix<T> &A, const MyMatrix<T> &B) {
-    unsigned i, j, k;
-    MyMatrix<T> C(A.getRow(), B.getCol());
-    for (i = 0; i < A.getRow(); i++) {
-        for (j = 0; j < B.getCol(); j++) {
-            for (k = 0; k < A.getCol(); k++) {
-                C[i][j] += A[i][k] * B[k][j];
-            }
-        }
-    }
-    return C;
-}
-
-template<typename T>
-MyMatrix<T> operator*(const T &e, const MyMatrix<T> &A) {
-    MyMatrix<T> C(A.getRow(), A.getCol());
-    for (unsigned i = 0; i < C.getRow(); i++) {
-        for (unsigned j = 0; j < C.getCol(); j++) {
-            C[i][j] = A[i][j] * e;
-        }
-    }
-    return C;
-}
-
-template<typename T>
-MyMatrix<T> operator/(const MyMatrix<T> &A, const T &e) {
-    MyMatrix<T> C(A.getRow(), A.getCol());
-    for (unsigned i = 0; i < C.getRow(); i++) {
-        for (unsigned j = 0; j < C.getCol(); j++) {
-            C[i][j] = A[i][j] / e;
-        }
-    }
-    return C;
 }
 
 #endif //MYCPPIMPLEMENT_MYMATRIX_H
