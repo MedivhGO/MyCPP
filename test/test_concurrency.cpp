d061e7f8bfce197783e3f93d2e672e55936fcfbe
MedivhGO <lijingqi93@gmail.com>
2024-07-09 11:36:00 +0800

    add some case

--- test/test_concurrency.cpp
+++ test/test_concurrency.cpp
@@ -45,8 +45,8 @@ TEST(MyCppConcurrency, test3) {
 }
 
 TEST(MyCppConcurrency, test4) {
-// std::promise常配合std::future使用。
-// 其作用是在一个线程中保存一个类型的值，可供相绑定的 std::future 对象在另一个线程中获取。
+    // std::promise 常配合 std::future 使用。
+    // 其作用是在一个线程中保存一个类型的值，可供相绑定的 std::future 对象在另一个线程中获取。
     std::packaged_task<int()> task([] { return 7; });
     std::future<int> f1 = task.get_future();
     std::thread t(std::move(task));
@@ -54,7 +54,7 @@ TEST(MyCppConcurrency, test4) {
     std::future<int> f2 = std::async(std::launch::async, [] { return 8; });
 
     std::promise<int> p;                  // 声明一个 promise 对象 p，其保存的值的类型为 int。
-    std::future<int> f3 = p.get_future(); // 声明一个 std::future 对象 f3，并通过 std::promise的get_future() 函数与 p 绑定。
+    std::future<int> f3 = p.get_future(); // 声明一个 std::future 对象 f3，并通过 std::promise 的 get_future() 函数与 p 绑定。
     std::thread([&p] { p.set_value_at_thread_exit(9); }).detach();
 
     std::cout << "Waiting..." << std::flush;
@@ -85,6 +85,31 @@ TEST(MyCppConcurrency, test4) {
 //  t1.join();
 //  t2.join();
 //}
+
+void func(std::promise<int> result_promise) noexcept {
+    result_promise.set_value(42);
+}
+
+TEST(MyCppConcurrency, test6) {
+    std::promise<int> result_promise;
+    std::future<int> result_future = result_promise.get_future();
+    std::thread t{func, std::move(result_promise)};
+    thread_guard g(t);
+    EXPECT_EQ(result_future.get(), 42);
+}
+
+void accm(int& count) {
+    count++;
+}
+
+TEST(MyCppConcurrency, test7) {
+    int count = 0;
+    std::once_flag of;
+    for (int i = 0; i < 10; ++i) {
+        std::call_once(of, accm, count);
+    }
+    EXPECT_EQ(count, 1);
+}
 
 
 
